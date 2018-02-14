f30f14626a747d687f2eed62230e5bf9f6218c0b
Jeffery Utter <jeff@jeffutter.com>
2018-02-14 17:13:39 -0600

    Mix New

---
+++ lib/nectar.ex
@@ -0,0 +1,18 @@
+defmodule Nectar do
+  @moduledoc """
+  Documentation for Nectar.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Nectar.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
